b003be244e6cdd0bfc07a495a178c9270cbbd0ad
Kevin Nielsen <keveran@gmail.com>
2023-10-21 13:45:28 +0200

    7 api watch identification endpoint (#9)
    * feat(api): classifyImage function
    
    * chore: add tfjs-image-node
    
    * chore(api): change export to not default
    
    * chore(deps): resolve
    
    * feat(wip): implement tfjs-image-node
    
    * feat(api): add api endpoint for identify
    
    * chore: remove model.json from public
    
    * chore: add package-lock.json

---
+++ app/api/identify/route.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
+import classifyImage from "tfjs-image-node";
+
+const modelURL = "https://teachablemachine.withgoogle.com/models/d1qL04bWG";
+
+interface IBody {
+  image: string;
+}
+
+interface IResult {
+  label: string;
+  probability: string;
+}
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  const metadataRes = await axios.get(
+    "https://teachablemachine.withgoogle.com/models/d1qL04bWG/metadata.json"
+  );
+  const metadata = await metadataRes.data;
+  try {
+    const body: IBody = await req.json();
+    const imageToClassify = body.image;
+
+    if (!imageToClassify)
+      return NextResponse.json(
+        { message: "ERR_MISSING_IMAGE" },
+        { status: 400 }
+      );
+
+    const result: IResult[] | Error = await classifyImage(
+      modelURL,
+      imageToClassify,
+      metadata
+    );
+    if (result instanceof Array) {
+      return NextResponse.json(result[0], { status: 200 });
+    } else {
+      return NextResponse.json({}, { status: 500 });
+    }
+  } catch (error) {
+    return NextResponse.json(error);
+  }
+}
