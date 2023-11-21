327fbd36a4901267100f0de5c6e0db233900bc64
kevinanielsen <kevinanielsen@outlook.com>
2023-11-21 15:33:19 +0100

    perf: implemented identify api endpoint

--- app/api/identify/route.ts
+++ app/api/identify/route.ts
@@ -1,44 +1,48 @@
-import axios from "axios";
+import { v2 as cloudinary } from "cloudinary";
 import { NextRequest, NextResponse } from "next/server";
 import classifyImage from "tfjs-image-node";
 
-const modelURL = "https://teachablemachine.withgoogle.com/models/d1qL04bWG";
+cloudinary.config({
+  cloud_name: "seikoidentifier",
+  api_key: "353591214627521",
+  api_secret: process.env.IMAGE_API_KEY,
+});
 
-interface IBody {
-  image: string;
-}
+const modelURL = "https://teachablemachine.withgoogle.com/models/d1qL04bWG";
 
 interface IResult {
   label: string;
   probability: string;
 }
 
-export async function POST(req: NextRequest, res: NextResponse) {
-  const metadataRes = await axios.get(
-    "https://teachablemachine.withgoogle.com/models/d1qL04bWG/metadata.json"
-  );
-  const metadata = await metadataRes.data;
+export async function POST(req: NextRequest) {
   try {
-    const body: IBody = await req.json();
-    const imageToClassify = body.image;
-
-    if (!imageToClassify)
+    const body = await req.json();
+    const image = body.image;
+    if (image.length === 0)
       return NextResponse.json(
         { message: "ERR_MISSING_IMAGE" },
         { status: 400 }
       );
 
-    const result: IResult[] | Error = await classifyImage(
-      modelURL,
-      imageToClassify,
-      metadata
+    const img = await cloudinary.uploader.upload(
+      image,
+      (error: any, result: any) => {
+        if (error) {
+          return NextResponse.json(error, { status: 500 });
+        }
+        return NextResponse.json(result);
+      }
     );
+
+    const result: IResult[] | Error = await classifyImage(modelURL, img.url);
+
     if (result instanceof Array) {
       return NextResponse.json(result[0], { status: 200 });
     } else {
       return NextResponse.json({}, { status: 500 });
     }
   } catch (error) {
-    return NextResponse.json(error);
+    return NextResponse.json(error, { status: 500 });
   }
 }
--- next.config.js
+++ next.config.js
@@ -1,13 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [
-      "cdn.watchcharts.com",
-      "avatars.githubusercontent.com",
-      "www.seikowatches.com",
-      "cdn2.jomashop.com",
-      "todicerg.sirv.com",
-    ],
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
   },
 };
 
