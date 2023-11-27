47bf1881c1bb07e85f8458a22035b0da5bcec77a
kevinanielsen <keveran@gmail.com>
2023-11-27 15:01:02 +0100

    chore: import authOptions from new file

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -1,5 +1,5 @@
+import { authOptions } from "@/utils/authOptions";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export default async function getSession() {
   const session = await getServerSession(authOptions);
