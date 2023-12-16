2222b575ed80edf8c789b36b9b16c9c227a0d344
kevinanielsen <keveran@gmail.com>
2023-12-16 19:46:26 +0100

    refactor: move gotResults function

---
+++ app/identify/util/gotResults.ts
@@ -0,0 +1,20 @@
+const gotResults = async (
+  error: Error,
+  results: { label: string; confidence: number }[]
+) => {
+  if (error) {
+    console.log(error);
+  }
+
+  if (results) {
+    const label = results[0].label; //Predicted label with highest confidence
+    const confidence = results[0].confidence
+      .toString()
+      .split("")
+      .splice(2, 2)
+      .join("");
+    return { label: label, confidence: Number(confidence) };
+  }
+};
+
+export default gotResults;
